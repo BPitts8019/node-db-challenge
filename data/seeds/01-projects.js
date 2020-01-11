44e923e60a98fc6c950414b4f148e0453ca4995b
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-10 18:12:49 -0700

    Added projects seed

---
+++ data/seeds/01-projects.js
@@ -0,0 +1,9 @@
+exports.seed = async knex => {
+   await knex("projects").insert([
+      {name: "project-01", description: "This is project 01", completed: 0},
+      {name: "project-02", description: "This is project 02", completed: 0},
+      {name: "project-03", description: "This is project 03", completed: 0},
+      {name: "project-04", description: "This is project 04", completed: 0},
+      {name: "project-05", description: "This is project 05", completed: 0}
+   ]);
+};
