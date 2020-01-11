7b3a175d2a775278086c3b708bc6d3c91631cac4
BPitts8019 <bradleyj.pitts@gmail.com>
2020-01-10 18:25:06 -0700

    Added tasks seed

---
+++ data/seeds/03-tasks.js
@@ -0,0 +1,22 @@
+exports.seed = async knex => {
+   await knex("tasks").insert([
+      {name: "Start It", description: "It's in the name!!", project_id: 1},
+      {name: "Do It", description: "Do the thing", project_id: 1},
+      {name: "Do It Again", description: "Do the thing again", project_id: 1},
+      {name: "Finish It", description: "You be done!", project_id: 1},
+      {name: "Start It", description: "It's in the name!!", project_id: 2},
+      {name: "Do It", description: "Do the thing", notes: "Do it quickly", project_id: 2},
+      {name: "Finish It", description: "You be done!", project_id: 2},
+      {name: "Start It", description: "It's in the name!!", project_id: 3},
+      {name: "Do It", description: "Do the thing", notes: "Do it quickly", project_id: 3},
+      {name: "Do It too", description: "Do the thing too", notes: "Do it mderately", project_id: 3},
+      {name: "Do It too 2", description: "Do the thing again", notes: "Do it slowly", project_id: 3},
+      {name: "Finish It", description: "You be done!", project_id: 3},
+      {name: "Start It", description: "It's in the name!!", project_id: 4},
+      {name: "Finish It", description: "You be done!", project_id: 4},
+      {name: "Start It", description: "It's in the name!!", project_id: 5},
+      {name: "Do It", description: "Do the thing", project_id: 5},
+      {name: "Do It Again", description: "Do the thing again", project_id: 5},
+      {name: "Finish It", description: "You be done!", project_id: 5},
+   ]);
+};
